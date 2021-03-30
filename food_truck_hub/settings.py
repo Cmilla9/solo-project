f48b34f0dcc41220e1921cbe1d2ccf93fabfa608
Cmilla9 <74386326+Cmilla9@users.noreply.github.com>
2021-03-30 12:22:02 -0700

    Update settings.py
    took out api key

--- food_truck_hub/settings.py
+++ food_truck_hub/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$%1oc%h+ug@r%jm6*j9!jd_beut&fg3n$!6p0u+69l$c2*8#(%'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
